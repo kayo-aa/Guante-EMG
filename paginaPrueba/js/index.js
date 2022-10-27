216e7018b6513164f374338f208f23a4a3908cb4
kayo <kaytoarcealcaraz@gmail.com>
2022-10-27 19:53:12 -0300

    pagina v1.0

--- paginaPrueba/js/index.js
+++ paginaPrueba/js/index.js
@@ -1,11 +1,32 @@
-async function getData() {
-  return fetch("http://192.168.0.128/content", {
+import { configLsDGH } from "./doughnut.js";
+import { configLsBar } from "./bar.js";
 
-  }).then((response) => response.json());
-}
+Chart.defaults.font = {
+  family: "'Ubuntu', 'Helvetica', 'Arial', sans-serif",
+  size: 16,
+  style: "normal",
+  lineHeight: 1.2,
+  weight: null,
+};
+Chart.defaults.color= "#FFF"
 
-let data = JSON.stringify(await getData().then());
+const chartFingerLs = new Chart(
+  document.getElementById("ls--fingers"),
+  configLsBar
+);
+const chartTotalLs = new Chart(
+  document.getElementById("ls--total"),
+  configLsDGH
+);
 
-let p= document.getElementById('content');
+export const chartFingerEsp = new Chart(
+  document.getElementById("esp--fingers"),
+  configLsBar
+);
+export const chartTotalEsp = new Chart(
+  document.getElementById("esp--total"),
+  configLsDGH
+);
 
-p.textContent = data
+
+
